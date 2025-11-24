bcb2ce1a9030ad8a59e56a07b77e52541b3de441
YukihitoJokyu <yukihito@YukihitoJokyunoMacBook-Air.local>
2025-11-24 17:55:28 +0900

    🔧 feat: shadcn/uiを導入し、関連ライブラリを追加
    shadcn/uiを利用してUIコンポーネントを構築するための初期設定を行いました。
    tailwindcssおよびviteの設定を更新し、パスエイリアス  を追加しました。
    radix-ui, lucide-reactなどの依存ライブラリをインストールしました。

--- dev/frontend/vite.config.ts
+++ dev/frontend/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
