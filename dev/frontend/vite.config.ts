782244f7f087caa0f5be48a1e4bc084730523e4d
YukihitoJokyu <yukihito@Mac.AirPort>
2025-11-30 00:21:07 +0900

    🔧 ビルド設定を更新
    shadcn/ui や react-router の導入に伴い、tailwind.config.js と vite.config.ts を更新しました。

--- dev/frontend/vite.config.ts
+++ dev/frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { TanStackRouterVite } from "@tanstack/react-router-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: true,
   },
